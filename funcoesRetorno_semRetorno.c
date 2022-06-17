4d47b912780b82cfec553c35b1510765ee2ad14e
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-06-17 03:47:22 -0300

    Add files via upload

---
+++ funcoesRetorno_semRetorno.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    float fmedia(float n1, float n2){//função recebe n1,n2 e retorna float
+    float resultado;
+    resultado = (n1 + n2)/2;
+    return(resultado);//retornando o valor para main
+    }
+int main(void){
+    float v1, v2, media;
+    printf("Digite o primeiro valor: ");
+    scanf("%f", &v1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%f", &v2);
+
+    media = fmedia(v1,v2);//chama a função e recebe o retorno
+
+    printf("Media = %5.2f\n", media);
+
+
+
+
+    return 0;
+}
