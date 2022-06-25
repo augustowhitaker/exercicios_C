5663569c043086681c2d9218bf1b51efd6477ccf
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-06-25 05:42:53 -0300

    Update teste05.c

--- teste05.c
+++ teste05.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// Escreva um programa que leia um vaetor A com 5 elementos numéricos.Contruir um vetor B onde cada elemento de B é o quadrado do elemento correspondente de A. Apresentar os vetores no final
+// Escreva um programa que leia um vetor A com 5 elementos numéricos.Contruir um vetor B onde cada elemento de B é o quadrado do elemento correspondente de A. Apresentar os vetores no final
 
 int main(){
 
