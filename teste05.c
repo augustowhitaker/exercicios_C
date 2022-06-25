5a64b2f98a0fce62e66e0be98fa853fe1e946af4
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-06-25 05:40:40 -0300

    Add files via upload

---
+++ teste05.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// Escreva um programa que leia um vator A com 5 elementos numéricos.Contruir um vetor B onde casa elemento de B é o quadrado do elemento correspondente de A. Apresentar os vetores no final
+
+int main(){
+
+    int vetA[5],vetB[5];
+
+    // lendo e calculando os vetores
+    for(int i = 0; i < 5; i++){
+        printf("Digite o valor de vetA [%d] = ", i + 1);
+        scanf("%d",&vetA[i]);
+
+        vetB[i] = vetA[i] * vetA[i];
+
+    }
+    // mostrando na tela o resultado
+
+    for(int i = 0; i < 5; i++){
+        printf("%d * %d = %d \n",vetA[i],vetA[i],vetB[i]);
+    }
+
+    return 0;
+}
