fd02bab5a95a1bc6f825b4c0f769e114fd520c89
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-07-01 03:08:49 -0300

    Add files via upload

---
+++ matriz1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#define TAM_MAX_LIN 5 //cinco alunos
+#define TAM_MAX_COL 5 //cinco notas
+
+int main() {
+
+
+	int lin, col;
+	float notas[TAM_MAX_LIN][TAM_MAX_COL], media;
+
+//entrada de dados
+		for (lin=0; lin<TAM_MAX_LIN; lin++) //controle do aluno
+		
+				for (col=0; col<TAM_MAX_COL; col++) { //entrada das notas (no exemplo 10 notas por aluno)
+				
+
+					       printf("Informe a nota %d do aluno %d \n", col+1,
+					              lin+1);
+					       scanf("%f", &notas[lin][col]);
+				}
+		for (lin=0; lin<TAM_MAX_LIN; lin++) { //passo para calcular media de cada aluno
+
+			media=0;
+			for (col=0; col<TAM_MAX_COL; col++) //soma todas as notas do aluno
+			
+					media+=notas[lin][col];
+			printf("A media do aluno %d foi de %.2f\n", lin+1,
+			       (float)media/TAM_MAX_COL);
+	
+    }
+ }
