6cc2f111fa3ce3586f4688222a111808a46c0142
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-07-05 06:05:43 -0300

    Update notasMatrizVetor.c

--- notasMatrizVetor.c
+++ notasMatrizVetor.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-// Crie um programa que pede que o usuário digite 4 notas de 0 a 10 (tipo float) de 4 alunos e os valores deverão ser armazenados em uma matriz bidimensional. Faça a média das notas de cada aluno em armazene as 4 médias em um vetor. Por ultimo  coloque na tela do usuário uma mensagem informando a média de cada aluno
+// Crie um programa que pede que o usuário digite 4 notas de 0 a 10 (tipo float) de 4 alunos e os valores deverão ser armazenados em uma matriz 
+//bidimensional. Faça a média das notas de cada aluno em armazene as 4 médias em um vetor. Por ultimo  coloque na tela do usuário uma mensagem 
+//informando a média de cada aluno
 
 
 int main(void){
@@ -24,7 +26,8 @@ int main(void){
         // Guardando as notas e calculando a media em um vetor
         mediaAlunos [aluno] = media / bimestresAnuais;
 
-        //  resetando a variavel media,pois senao irá ser somado a nota do aluno 1 com o do aluno 2...,para isso não ocorrer e calcular aluno por aluno resetamos a variavel
+        //  resetando a variavel media,pois senao irá ser somado a nota do aluno 1 com o do aluno 2...,para isso não ocorrer e calcular aluno por aluno
+        //resetamos a variavel
         media = 0;//como esta dentro do laço for toda vez que mudar de aluno ele limpa a variavel
 
         
