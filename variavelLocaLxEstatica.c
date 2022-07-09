46be7ea52a35610604ba44022a7be6a69ee3d493
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-07-09 05:29:53 -0300

    Update variavelLocaLxEstatica.c

--- variavelLocaLxEstatica.c
+++ variavelLocaLxEstatica.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 void teste(void){
+    //variavel automatica é apenas armazenadas dentro do escopo da função, ou seja toda vez que é invocada ela retorna o mesmo resultado pois ela
+    //"destruida" toda vez que roda, sendo assim usada para manter o mesmo retorno
     int variavelLocalAutomatica = 2;
     variavelLocalAutomatica *= 2;
     printf("Local automatica  = %d \n", variavelLocalAutomatica);
-
+    
+    //static é usada para manter a funcao de modo acumalativo como no exemplo abaixo,ou seja ela não é "destruida" toda vez que chamamos, ela vai
+    //guardando o resultado e adicionando a cada vez que invocamos ela em uma função
     static int variavelLocalEstatica = 2;
     variavelLocalEstatica *= 2;
     printf("Local Estatica = %d \n" , variavelLocalEstatica);
