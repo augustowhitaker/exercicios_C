aba9eb7eeadbbf50df1c71b7d80de6fd5630b00f
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-07-09 04:54:29 -0300

    Update exemploFuncao.c

--- exemploFuncao.c
+++ exemploFuncao.c
@@ -2,7 +2,9 @@
 
 
 int main(){
-    float calcularAreaRetangulo(float x, float y);//chamando a funcao no main,porem se a funcao for criada acima da funcao main e não abaixo não seria necessario invocar  ela aqui, bastaria fazer apenas o codigo abaixo.
+    float calcularAreaRetangulo(float x, float y);//chamando a funcao no main,porem se a funcao for criada acima da funcao main e 
+    //não abaixo não seria necessario invocar  ela aqui, bastaria fazer apenas o codigo abaixo.
+    
     float area = calcularAreaRetangulo(10.0, 20.0);//criado a variavel area para armazenar a funcao de calculo areaRetangulo
 
     printf("A area eh %f " , area);
