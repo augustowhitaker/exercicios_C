4855a218535db04cfd3b54b90c1dea5f4f4ff31a
Augusto Whitaker Felix dos Santos <61566660+augustowhitaker@users.noreply.github.com>
2022-07-11 01:23:54 -0300

    Update funcaoChamandoOutraFuncao.c

--- funcaoChamandoOutraFuncao.c
+++ funcaoChamandoOutraFuncao.c
@@ -4,14 +4,15 @@ float valorAbsoluto (float x){//funcao para deixar positivo os numeros
    
         return x * -1;//retorno contendo o calculo negativo para positivo
 }
-float somaDeDigitos(float num1, float num2){//fazendo a soma e invocando a outra funcao "valorAbsoluto" quando necessario
+float somaDeDigitos(float num1, float num2){//Função de soma  
 
     if(num1 < 0){//testando se o valor é negativo
-        num1 = valorAbsoluto(num1);//convertando o numero para positivo se necessario
+        num1 = valorAbsoluto(num1);//invocando a outra funcao "valorAbsoluto" quando necessario e passando o valor para positivo
     }
-    if(num2 < 0){
-        num2 = valorAbsoluto(num2);//perceba que é colocado dentro do parametro da funcao do valorAbsoluto o proprio num2/num1, pois agora os nums 
-                                   //passarão a valer o num de dentro do parametro
+    if(num2 < 0){//testando se o valor é negativo
+        num2 = valorAbsoluto(num2);//invocando a outra funcao "valorAbsoluto" quando necessario perceba que é colocado dentro do parametro da funcao do 
+                                   //valorAbsoluto o proprio num2/num1, pois agora os nums passarão a valer o num de dentro do parametro
+                                   
     }
     return num1 + num2;//retornando a soma
 }
